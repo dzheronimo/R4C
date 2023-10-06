18c528b9672f96f82590d0146b30d0dcce01c0cd
Dzheronimo <dzheronimo@me.com>
2023-10-06 15:44:49 +1000

    изменены объявления приложений

--- R4C/settings.py
+++ R4C/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'customers',
-    'orders',
-    'robots'
+    'customers.apps.CustomersConfig',
+    'orders.apps.OrdersConfig',
+    'robots.apps.RobotsConfig'
 ]
 
 MIDDLEWARE = [
