33e2858c15a777c7d13da28b6ecb67daf0584af1
Dzheronimo <dzheronimo@me.com>
2023-10-10 01:18:43 +1000

    В сериализаторе NewRobotSerializer переодпределен метод create: при создании модели, высчитывается поле serial(model+version). Так же это поле добавлено в сам сериализатор

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,27 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from robots.models import Robot
 
 
 class NewRobotSerializer(ModelSerializer):
+    serial = SerializerMethodField()
 
     class Meta:
         model = Robot
-        fields = ('model', 'version', 'created')
+        fields = ('serial', 'model', 'version', 'created')
+
+    def get_serial(self, obj):
+        return f'{obj.model}{obj.version}'
+
+    def create(self, validated_data):
+        model = validated_data.get('model')
+        version = validated_data.get('version')
+        created = validated_data.get('created')
+        serial = f'{model}{version}'
+
+        return Robot.objects.create(
+            model=model,
+            version=version,
+            created=created,
+            serial=serial
+        )
