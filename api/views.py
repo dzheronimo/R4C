c6e0d6f7e3395bc0e9bc014db6a6a59fefbae004
Dzheronimo <dzheronimo@me.com>
2023-10-10 14:49:04 +1000

    Вьюсет NewRobot, теперь собран из миксина Create. Добавлен permission class (admin)

--- api/views.py
+++ api/views.py
@@ -1,16 +1,16 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.mixins import CreateModelMixin
+from rest_framework.viewsets import GenericViewSet
 
 from orders.models import WaitingList, Order
 from robots.models import Robot
 from .serializers import NewRobotSerializer
 
 
-class NewRobot(ModelViewSet):
+class NewRobot(CreateModelMixin, GenericViewSet):
     queryset = Robot.objects.all()
     serializer_class = NewRobotSerializer
+    permission_classes = [IsAdminUser]
 
     class Meta:
         model = Robot
-
-
-
